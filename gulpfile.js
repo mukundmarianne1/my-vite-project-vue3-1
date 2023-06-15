cbe01818d6337273cb34258286e010cf1717d861
mukundmarianne1 <136606973+mukundmarianne1@users.noreply.github.com>
2023-06-15 21:10:32 +0800

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+/**
+ *  1、安装nodejs；
+    2、新建package.json文件；
+    3、全局和本地安装gulp；
+    4、安装gulp插件；
+    5、新建gulpfile.js文件；
+    6、通过命令提示符运行gulp任务。
+ * 全局安装gulp
+ * npm install --global gulp
+ * 安装依赖项
+ * npm install gulp-jshint gulp-sass gulp-concat gulp-uglify gulp-rename --save-dev
+ * 运行gulp
+ * gulp
+ */
+
+// 引入 gulp
+var gulp        = require('gulp');
+// 引入组件
+var sass        = require('gulp-sass');// Sass 编译成 CSS
+var prefix      = require('gulp-autoprefixer');//根据设置浏览器版本自动处理浏览器前缀
+var minifyCSS   = require('gulp-minify-css'); //压缩css
+var rename      = require('gulp-rename'); //重命名文件
+var browserSync = require('browser-sync').create();// 实时响应，自动刷新
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+});
+
+// Build css files
+gulp.task('compressCSS', function() {
+    gulp.src('src/css/*.scss')
+        .pipe(sass())
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(minifyCSS())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.stream());
+});
+
+// Watch files for changes & recompile
+gulp.task('watch', function () {
+    gulp.watch(['src/css/*.scss'], ['compressCSS']);
+});
+
+// Default task, running just `gulp` will move font, compress js and scss, start server, watch files.
+gulp.task('default', ['compressCSS', 'browser-sync', 'watch']);
